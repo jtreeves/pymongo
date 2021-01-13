88ad3962ea09e9d53ccfa06de4a31e53ef27e0d6
jtreeves <jackson.t.reeves@gmail.com>
2021-01-13 17:02:14 -0500

    [github] Build out skeleton for classmates version

--- github.py
+++ github.py
@@ -1,6 +1,22 @@
 from pymongo import MongoClient
 import requests, datetime, pprint
 
-response = requests.get('https://api.github.com/users/facebook')
+simone_data = requests.get('https://api.github.com/users/facebook')
+jenny_data = requests.get('https://api.github.com/users/facebook')
+alan_data = requests.get('https://api.github.com/users/facebook')
 
-print(response.json().get('bio'))
+simone_obj = {
+    'bio': simone_data.json().get('bio')
+}
+
+jenny_obj = {
+    'bio': simone_data.json().get('bio')
+}
+
+alan_obj = {
+    'bio': simone_data.json().get('bio')
+}
+
+print(simone_obj)
+print(jenny_obj)
+print(alan_obj)
