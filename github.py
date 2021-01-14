91842caeaf43b7a4399f91fd80f6a98c201b86cf
jtreeves <jackson.t.reeves@gmail.com>
2021-01-13 19:20:28 -0500

    [github] Build out objects with classmates' data

--- github.py
+++ github.py
@@ -1,22 +1,37 @@
 from pymongo import MongoClient
 import requests, datetime, pprint
 
-simone_data = requests.get('https://api.github.com/users/facebook')
-jenny_data = requests.get('https://api.github.com/users/facebook')
-alan_data = requests.get('https://api.github.com/users/facebook')
+simone_data = requests.get('https://api.github.com/users/sschneeberg')
+jenny_data = requests.get('https://api.github.com/users/ruvvet')
+jeremy_data = requests.get('https://api.github.com/users/jjuriz')
 
 simone_obj = {
-    'bio': simone_data.json().get('bio')
+    'id': simone_data.json().get('id'),
+    'name': simone_data.json().get('name'),
+    'bio': simone_data.json().get('bio'),
+    'location': simone_data.json().get('location'),
+    'followers': simone_data.json().get('followers'),
+    'date': datetime.date.day
 }
 
 jenny_obj = {
-    'bio': simone_data.json().get('bio')
+    'id': jenny_data.json().get('id'),
+    'name': jenny_data.json().get('name'),
+    'bio': jenny_data.json().get('bio'),
+    'location': jenny_data.json().get('location'),
+    'followers': jenny_data.json().get('followers'),
+    'date': datetime.date.day
 }
 
-alan_obj = {
-    'bio': simone_data.json().get('bio')
+jeremy_obj = {
+    'id': jeremy_data.json().get('id'),
+    'name': jeremy_data.json().get('name'),
+    'bio': jeremy_data.json().get('bio'),
+    'location': jeremy_data.json().get('location'),
+    'followers': jeremy_data.json().get('followers'),
+    'date': datetime.date.day
 }
 
 print(simone_obj)
 print(jenny_obj)
-print(alan_obj)
+print(jeremy_obj)
